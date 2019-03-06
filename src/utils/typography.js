0368a99c2ef36e7e56311a4e3efcd24de23d920f
ShizukuIchi <shizukuichi@gmail.com>
2019-03-06 10:57:20 +0800

    windows font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,20 +1,28 @@
 import Typography from "typography"
 import Wordpress2016 from "typography-theme-wordpress-2016"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-    a: {
-      color: "#199bd4",
-    },
-  }
-}
-
 delete Wordpress2016.googleFonts
 
-const typography = new Typography(Wordpress2016)
+const overrideOptions = {
+  bodyFontFamily: ["微軟正黑體"],
+  headerFontFamily: ["微軟正黑體"],
+  overrideThemeStyles() {
+    return {
+      "a.gatsby-resp-image-link": {
+        boxShadow: `none`,
+      },
+      a: {
+        color: "#199bd4",
+        fontFamily: "微軟正黑體",
+      },
+      "h1,h2,h3,h4,h5,h6": {
+        fontFamily: "微軟正黑體",
+      },
+    }
+  },
+}
+
+const typography = new Typography({ ...Wordpress2016, ...overrideOptions })
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
