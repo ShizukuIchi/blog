379acfaec92f229246a242845c98cdf3a481af67
ShizukuIchi <shizukuichi@gmail.com>
2019-03-12 14:26:55 +0800

    feat: code highlight

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -11,6 +11,29 @@ const overrideOptions = {
       "a.gatsby-resp-image-link": {
         boxShadow: `none`,
       },
+      ".gatsby-highlight-code-line": {
+        backgroundColor: "#feb",
+        display: "block",
+        marginRight: "-1em",
+        marginLeft: "-1em",
+        paddingRight: "1em",
+        paddingLeft: "0.75em",
+        borderLeft: "0.25em solid #f99",
+      },
+      ".gatsby-highlight": {
+        backgroundColor: "#fdf6e3",
+        borderRadius: "0.4em",
+        margin: "0.5em 0",
+        overflow: "auto",
+      },
+      'gatsby-highlight pre[class*="language-"]': {
+        backgroundColor: "transparent",
+        margin: "0",
+        padding: "0",
+        overflow: "initial",
+        float: "left",
+        minWidth: "100%",
+      },
       a: {
         color: "#199bd4",
         fontFamily: "微軟正黑體",
